9b41641b2aba189b6c8700ca5c616704d7d65066
JaevillenOliveira <jaevillenks@gmail.com>
2020-04-21 17:22:16 -0300

    Separa os dados baixados do INPE em arquivos por bioma por mês

---
+++ Dados_coletados/Dados_INPE_porBioma/Filter.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+
+import pandas as pd
+
+mainFunction()
+
+def mainFunction():   
+    biomes = ["Amazonia", "Caatinga", "Cerrado", "MataAtlantica", "Pampa", "Pantanal"]
+    for b in biomes:
+        print('b')
+        for beginningYear in range (2016, 2019):
+            # Name pattern Focos'Biome'_'beginningYear'-'endingYear'.csv
+            InFileName = 'Focos'+b+'_'+str(beginningYear)+'-'+str(beginningYear+1)+'.csv' 
+            inputFile = pd.read_csv(InFileName, usecols=['datahora', 'diasemchuva', 'precipitacao', 'riscofogo', 'latitude', 'longitude'])
+            
+            outFileName = 'FocosPorMes/Focos'+b+'_'+str(beginningYear)+'_'+str(12)+'.csv' 
+            func1(inputFile, beginningYear, 12, outFileName)
+            for month in range (1, 12):
+                if(month < 10):
+                    outFileName = 'FocosPorMes/Focos'+b+'_'+str(beginningYear+1)+'_'+'0'+str(month)+'.csv' 
+                else:
+                    outFileName = 'FocosPorMes/Focos'+b+'_'+str(beginningYear+1)+'_'+str(month)+'.csv' 
+                func1(inputFile, beginningYear, month, outFileName)
+                
+        
+def func1(inputFile, year, month, outFileName):
+    out = inputFile[(inputFile.datahora.str.startswith(str(year)+'/'+str(month))) &
+                    (inputFile.latitude>=-90) & (inputFile.latitude<=90) &
+                    (inputFile.longitude>=-180) & (inputFile.longitude<=180)]
+    
+    print('a')
+     
+    out.to_csv(outFileName)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
